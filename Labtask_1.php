2cbc3cc17e1ee84967f651374d08dea7b0c929a6
RagibShahriar01 <shahriarragib7@gmail.com>
2025-11-06 01:21:03 +0600

    first commit

---
+++ Labtask_1.php
@@ -0,0 +1,67 @@
+<body>
+
+<center>
+    <h1 style= "color:blue">Clinic Patient Registration</h1>
+
+
+<br>
+<br>
+<br>
+
+    Full Name:
+    <input type= "text"><br>
+    <br>
+
+
+    Age:
+    <input type= "text"><br>
+    <br>
+
+    Phone Number:
+    <input type= "text"><br>
+    <br>
+
+    Email Address:
+    <input type= "text"><br>
+    <br>
+
+    Insurance Provider:
+
+    <select name="">
+        <option> CBL</option>
+        <option> DBL</option>
+        <option> Dhaka Bank</option>
+</select>
+<br>
+<br>
+
+    Insurance Policy Number:
+    <input type= "text"><br>
+    <br>
+
+</center>
+
+<center>
+<h1 style= "color:blue">Additional Information</h1>
+
+
+    Username:
+    <input type= "text"><br>
+    <br>
+
+
+
+    Password:
+    <input type= "text"><br>
+    <br>
+
+
+    Confirm Password:
+    <input type= "text"><br>
+    <br>
+
+
+    <input type= "button" value="Register" style="background-color: blue; color: white;"> <br>
+
+</center>
+</body>
